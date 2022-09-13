face361f96ddd2a3758a4c1e0551270ae931403d
AlessioSonego <sonegus.22@gmail.com>
2022-09-13 15:39:45 +0300

    little changes, comments and file moved

--- src/js/saveFormData/personalInfo.js
+++ src/js/saveFormData/personalInfo.js
@@ -1,17 +1,44 @@
 /**
- * Save data of personal info form in localStorage
+ * Save data of personal info form in localStorage:
+ * 
+ * name
+ * surname
+ * phoneNumber
+ * email
+ * nationality
+ * birthDate
+ * gender
+ * motherLanguage
+ * country
+ * city
+ * address
  */
 
 const personalInfoSaver = () => {
-    localStorage.setItem("name", doucment.getElementById("name"));
-    localStorage.setItem("surname", document.getElementById("surname"));
-    localStorage.setItem("phoneNumber", document.getElementById("phoneNumber"));
-    localStorage.setItem("email", document.getElementById("email"));
-    localStorage.setItem("nationality", document.getElementById("nationality"));
-    localStorage.setItem("birthDate", document.getElementById("birthDate"));
-    localStorage.setItem("gender", document.getElementById("gender"));
-    localStorage.setItem("motherLanguage", document.getElementById("motherLanguage"));
-    localStorage.setItem("country", document.getElementById("country"));
-    localStorage.setItem("city", document.getElementById("city"));
-    localStorage.setItem("address", document.getElementById("address"));
+
+    // store all data in variables
+    var name = document.getElementById("name");
+    var surname = document.getElementById("surname");
+    var phoneNumber = document.getElementById("phoneNumber");
+    var email = document.getElementById("email");
+    var nationality = document.getElementById("nationality");
+    var birthDate = document.getElementById("birthDate");
+    var gender = document.getElementById("gender");
+    var motherLanguage = document.getElementById("motherLanguage");
+    var country = document.getElementById("country");
+    var city = document.getElementById("city");
+    var address = document.getElementById("address");
+
+    // store data from variables in localStorage
+    localStorage.setItem("name", name);
+    localStorage.setItem("surname", surname);
+    localStorage.setItem("phoneNumber", phoneNumber);
+    localStorage.setItem("email", email);
+    localStorage.setItem("nationality", nationality);
+    localStorage.setItem("birthDate", birthDate);
+    localStorage.setItem("gender", gender);
+    localStorage.setItem("motherLanguage", motherLanguage);
+    localStorage.setItem("country", country);
+    localStorage.setItem("city", city);
+    localStorage.setItem("address", address);
 }
