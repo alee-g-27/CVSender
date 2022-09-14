d9842fc158c0d36aac4d213b1ddf2bfe9550d967
AlessioSonego <sonegus.22@gmail.com>
2022-09-14 14:43:23 +0300

    Update educationInfo

--- src/js/saveFormData/educationInfo.js
+++ src/js/saveFormData/educationInfo.js
@@ -1,6 +1,24 @@
 /**
  * Save data of education info form in localStorage
+ * 
+ * title
+ * organisation
+ * address
+ * postalCode
+ * city
+ * country
+ * website
+ * fromDate
+ * toDate
  */
 const educationDataSaver = () => {
-    
+    var title = document.getElementById("title");
+    var organisation = document.getElementById("organisation");
+    var address = document.getElementById("address");
+    var postalCode = document.getElementById("postalCode");
+    var city = document.getElementById("city");
+    var country = document.getElementById("country");
+    var website = document.getElementById("website");
+    var fromDate = document.getElementById("fromDate");
+    var toDate = document.getElementById("toDate");x
 }
